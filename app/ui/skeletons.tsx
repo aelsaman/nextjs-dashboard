35a113fd92a04fc81e9537f5fa925135b03cc7ab
Alaa ElSaman <aelsaman0x@gmail.com>
2024-02-03 21:10:54 +0200

    code cleaning

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -7,12 +7,12 @@ export function CardSkeleton() {
     <div
       className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
     >
-      <div className="flex p-4">
+      <div className="	flex p-4">
         <div className="h-5 w-5 rounded-md bg-gray-200" />
-        <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
+        <div className="	ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
       </div>
-      <div className="flex items-center justify-center truncate rounded-xl bg-white px-4 py-8">
-        <div className="h-7 w-20 rounded-md bg-gray-200" />
+      <div className="	flex items-center justify-center truncate rounded-xl bg-white px-4 py-8">
+        <div className="	h-7 w-20 rounded-md bg-gray-200" />
       </div>
     </div>
   );
@@ -82,11 +82,11 @@ export function LatestInvoicesSkeleton() {
   );
 }
 
-export default function DashboardSkeleton() {
+export function DashboardSkeleton() {
   return (
     <>
       <div
-        className={`${shimmer} relative mb-4 h-8 w-36 overflow-hidden rounded-md bg-gray-100`}
+        className={` ${shimmer} relative mb-4 h-8 w-36 overflow-hidden rounded-md bg-gray-100`}
       />
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <CardSkeleton />
